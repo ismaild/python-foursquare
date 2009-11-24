90e91c118b4d27763dc5e8e0c5b04af7fc9d987c
Ismail Dhorat <idhorat@gmail.com>
2009-11-24 19:38:35 +0200

    Implemented basic auth and additional methods

--- foursquare.py
+++ foursquare.py
@@ -1,10 +1,11 @@
 import urllib2
 import urllib
+import base64
 import simplejson
 
 class foursquare():
     """
-    foursquare.py - v0.1.2
+    foursquare.py - v0.1.
 
     A simple python wrapper for the foursquare API.
 
@@ -16,37 +17,38 @@ class foursquare():
     Usage:
     foursquare.method(requiredargs, optionalargs)
     
-    Supported Methods:
+    foursquare.get_cities()    
+    foursquare.get_venues(-27.091874,29.057225)
+    foursquare.get_venues(-27.091874,29.057225, l=10)
+    
+    * Optional args are keyword arguments and the keyword is mapped to the get param needed by the foursquare api
     """
     def __init__(self):
-        self.base_url = 'http://api.foursquare.com'
-        self.api_version = 'v1'
-        self.url = self.base_url + '/' + self.api_version + '/'
+        self.url = 'http://api.foursquare.com/v1/'
         self.output = '.json'
-        self.endpoints = {
-            'test': 'test', 
-            'cities': 'cities',
-            'venues': 'venues',
-            'tips' : 'tips',
-            'checkcity': 'checkcity',
-            'venue': 'venue'
-        }
     
-    def _return_result(self, endpoint, params=None):
+    def _return_result(self, endpoint, username=None, password=None, params=None):
         """
         Internal method to return the results
         
-        Args: 
-        endpoint -- required (What kind of request is this?)
-        params -- optional (a dictionary of get params and values)
+        Args (required): 
+        - endpoint 
+        
+        keyword args(optional):
+        - username
+        - password
+        - params
         """
-        endpoint = self.endpoints[endpoint]
         query_url = self.url + endpoint + self.output
         if params:
             query_url = (query_url + '?%s') % urllib.urlencode(params)
-        print query_url
-        result = simplejson.load(urllib2.urlopen(query_url))
-        return result
+        request = urllib2.Request(query_url)
+
+        if username and password:
+            b64 = base64.encodestring('%s:%s' % (username, password))[:-1]
+            authheader="Basic %s" % b64
+            request.add_header('Authorization', authheader)
+        return simplejson.load(urllib2.urlopen(request))
         
     def test(self):
         """ 
@@ -73,7 +75,7 @@ class foursquare():
         """
         return self._return_result('cities')
     
-    def get_venues(self, lat, lon, search='', limit=''):
+    def get_venues(self, lat, lon, **kwargs):
         """
         Returns venues within range for a given lat & lon
         
@@ -81,44 +83,101 @@ class foursquare():
         latitude -- Required
         longitude -- Required
         
-        keyword arguments:
-        limit -- optional 
-        search -- optional
+        keyword args (Optional):
+        l=10 --- Limit results
+        q=food --- search for keyword
         """
-        params = {'geolat': lat, 'geolong': lon, 'q': search, 'l': limit}
-        return self._return_result('venues', params=params)
+        kwargs['geolat'] = lat
+        kwargs['geolong'] = lon
+        return self._return_result('venues', params=kwargs)
         
-    def get_tips(self, lat, lon, limit=''):
+    def get_tips(self, lat, lon, **kwargs):
         """
         Returns tips within range of a given lat & lon
         
-        args: 
-        latitude -- Required
-        longitude -- Required
+        args (required): 
+        - latitude
+        - longitude
         
-        keyword arguments:
-        limit -- optional
+        Keyword Args (optional):
+        - l=10
         """
-        params = {'geolat': lat, 'geolong': lon, 'l': limit}
-        return self._return_result('tips', params=params)
+        kwargs['geolat'] = lat
+        kwargs['geolong'] = lon
+        return self._return_result('tips', params=kwargs)
     
     def check_city(self, lat, lon):
         """
         Returns the closest foursquare city for a give lat & lon
         
-        args:
-        lat - required
-        lon - required
+        args (required):
+        - lat
+        - lon
         """
         params = {'geolat': lat, 'geolong': lon}
         return self._return_result('checkcity', params=params)
-
-    def get_venue_details(self, vid):
+    
+    def get_venue_detail(self, vid , username=None, password=None,):
         """
         Returns detailed info for a specific venue
         
-        args:
-        venuid
+        args (required):
+        - vid 
+        
+        keyword args (optional):
+        - username
+        - password
         """
         params = {'vid': vid }
-        return self._return_result('venue', params=params)
+        return self._return_result('venue', username=username, password=password, params=params)
+    
+    def get_history(self, username, password):
+        """
+        Returns a history for the authenticated user
+        
+        args (required):
+        - username
+        - password
+        """
+        return self._return_result('history', username=username, password=password)
+    
+    def get_user_detail(self, username, password, **kwargs):
+        """
+        Returns user details for a given uid or authenticated user
+        
+        args (Required):
+        - username 
+        - password
+        
+        Keyword Arguments (optional):
+        - uid=xxx 
+        - mayor=1
+        - bages=1
+        """
+        return self._return_result('user', username=username, password=password, params=kwargs)
+    
+    def get_friends(self, username, password, **kwargs):
+        """
+        Returns friends for a given uid or authenticated user
+        
+        Args (required):
+        - username
+        - password
+        
+        keyword args (optional):
+        - uid=12345
+        """
+        return self._return_result('friends', username=username, password=password, params=kwargs)
+    
+    def get_checkins(self, username, password, **kwargs):
+        """
+        Returns checkins for friends of authenticated user
+        
+        Args (required):
+        - username
+        - password
+        
+        keyword args (optional):
+        - cityid=12345
+        """
+        return self._return_result('checkins', username=username, password=password, params=kwargs)
